68090a698e2d0c01930735f955ad838d0183d578
chaya2209 <chaya3007.be20@chitkara.edu.in>
2022-08-11 14:44:45 +0530

    blogger application

--- scripts.js
+++ scripts.js
@@ -5,9 +5,18 @@ document.querySelector("form.searchbox").addEventListener("submit", (e) => {
   const url = `https://jsonplaceholder.typicode.com/posts?title=${data_search}`;
   //  console.log(searchValue);
   xhr.open("GET", url, true);
+  let text='';
+  var array={};
   xhr.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      console.log(JSON.parse(xhr.responseText));
+      //console.log(JSON.parse(xhr.responseText));
+      
+      const res=JSON.parse(xhr.responseText);
+      array={...res}
+      console.log(array[0].title);
+      console.log(array[0].body);
+      
+
     }
   };
 
