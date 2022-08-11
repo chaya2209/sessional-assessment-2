d54854335155869ae8fd909f0dfe6a9012e9374c
chaya2209 <chaya3007.be20@chitkara.edu.in>
2022-08-11 12:51:48 +0530

    blogger application

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
-
-
 document.querySelector('form.searchbox').addEventListener('submit', (e) => {
     e.preventDefault();
     const xhr = new XMLHttpRequest();
-    const searchValue = document.querySelector('#data-input');
-    console.log(searchValue.value);
+    const data_search = document.querySelector('#data-input').value;
+    
+  //  console.log(searchValue);
 
 
 });
