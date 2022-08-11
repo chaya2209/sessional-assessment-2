afa4504956ac97bc5bb0416844e739c045c07197
chaya2209 <chaya3007.be20@chitkara.edu.in>
2022-08-11 13:24:19 +0530

    blogger application

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
-document.querySelector('form.searchbox').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const xhr = new XMLHttpRequest();
-    const data_search = document.querySelector('#data-input').value;
-    
+document.querySelector("form.searchbox").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const xhr = new XMLHttpRequest();
+  const data_search = document.querySelector("#data-input").value;
+  const url = `https://jsonplaceholder.typicode.com/posts?title=${data_search}`;
   //  console.log(searchValue);
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      console.log(JSON.parse(xhr.responseText));
+    }
+  };
 
-
+  xhr.send();
 });
