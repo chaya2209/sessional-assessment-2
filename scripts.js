f0a0140fff6c0a0fd4270905c2362ee2b3785313
chaya2209 <chaya3007.be20@chitkara.edu.in>
2022-08-11 23:51:37 +0530

    blogger application

--- scripts.js
+++ scripts.js
@@ -1,37 +1,5 @@
 document.querySelector("form.searchbox").addEventListener("submit", (e) => {
- /* e.preventDefault();
-  const xhr = new XMLHttpRequest();
-  const data_search = document.querySelector("#data-input").value;
-  const url = `https://jsonplaceholder.typicode.com/posts?title=${data_search}`;
-  //  console.log(searchValue);
-  xhr.open("GET", url, true);
-  let text = "";
 
-  var array = {};
-
-
-  xhr.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      //console.log(JSON.parse(xhr.responseText));
-
-      const res = JSON.parse(xhr.responseText);
-      array = {...res};
-      var array_length = Object.keys(array).length;
-      let temp=''
-      for(let i=0;i<array_length;i++)
-      {
-        temp += `
-        <div class="blog-items" style="width: 20%; margin: 1.2em;">
-            <p style="color: gray">${array[i].title}</p>
-            <p style="color: gray">${array[i].body}</p>
-        </div>
-    `
-
-      }
-      
-      document.querySelector('my-container').innerHTML = '<p> temp </p> ';
-    }
-  };*/
   e.preventDefault();
 
   const xhr = new XMLHttpRequest();
